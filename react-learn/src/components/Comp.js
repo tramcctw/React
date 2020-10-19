2525bcf92649d20499faceb3fed800ddf91dc31d
tramcctw <tramcctw@foxmail.com>
2020-10-19 20:51:42 +0800

    commit

--- react-learn/src/components/Comp.js
+++ react-learn/src/components/Comp.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react'
 
-export class A extends Component {
+export default class Comp extends Component {
     render() {
         return (
-            <h1>A:{this.props.num}</h1>
-        )
-    }
-}
-
-
-export class B extends Component{
-    render() {
-        return (
-            <h1>B:{this.props.num}</h1>
+            <div>
+                
+            </div>
         )
     }
 }
--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,18 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {A,B} from './components/Comp'
-import withLog from './HOC/withLog'
-import withLogin from './HOC/withLogin'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Comp from './components/Comp'
 
-let LogA = withLog(A,'abc')
-let LogB = withLog(B,'bcd')
-let LA = withLogin(LogA)
-let LB = withLogin(LogB)
 
-ReactDOM.render(<div>
-  <LA login={true} num={1}/>
-  <LB login={true} num={2}/>
-</div>,
-  document.getElementById('root')
-);
-
+ReactDOM.render( <Comp/>,document.getElementById("#root"))
