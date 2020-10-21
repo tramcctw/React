e707f35477960a25bae60ad4909bfdff28bfabe4
saybot <saybot@saybotdeMac-mini.local>
2020-10-21 10:07:37 +0800

    commit

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,13 +1,9 @@
 import React,{useState} from 'react'
-import {Provider} from 'react-redux'
-import Counter from './components/Counter'
-import store from '../store'
-
 export default function App(){
     
     return (
-        <Provider store = {store} >
-            <Counter/>
-        </Provider>
+        <div>
+            hello
+        </div>
     )
 }
