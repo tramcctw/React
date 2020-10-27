fa14713c736bb986586f7df9bb22f64518391045
tramcctw <tramcctw@foxmail.com>
2020-10-28 00:30:10 +0800

    11

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 
 PureComponent是一个组件，如果某一个组件继承自该组件，则该组件的
 shouldComponentUpdate会进行优化，对属性和状态进行浅比较，如果相等则不会重新渲染
+
+
+**注意**
+1. PureComponent进行的是浅比较，
+    1. 为了效率，应该尽量使用PureComponent
+    2. 要求不要改动之前的状态，永远是创建的状态覆盖之前的状态
+    3. 有一个第三方js库，Immutable.js 它专门用于制作不可变对象
+2. 函数组件，使用React.memo函数制作纯组件
--- react-learn/src/test/Task.js
+++ react-learn/src/test/Task.js
@@ -3,27 +3,45 @@ import PropTypes from 'prop-types'
 import './Task.css'
 import equalObj from '../utils/equalObject'
 
-export default class Task extends Component {
-
-    static propTypes = {
-        name:PropTypes.string.isRequired,
-        isFinish:PropTypes.bool.isRequired
-    }
-
-    shouldComponentUpdate(nextProps,nextState){
-        if(equalObj(this.props,nextProps) && equalObj(this.state,nextState)){
-            return false
-        }
-        return true
-    }
-
-
-    render() {
-        console.log('Task render');
-        return (
-            <li className={this.props.isFinish ? 'finish':'default'}>
-                {this.props.name}                  
+function Task(props){
+    console.log("Task render");
+    return (
+            <li className={props.isFinish ? 'finish':'default'}>
+                {props.name}                  
             </li>
         )
-    }
 }
+
+Task.propTypes = {
+    name:PropTypes.string.isRequired,
+    isFinish:PropTypes.bool.isRequired
+}
+
+export default React.memo(Task)
+
+
+
+// export default class Task extends Component {
+
+//     static propTypes = {
+//         name:PropTypes.string.isRequired,
+//         isFinish:PropTypes.bool.isRequired
+//     }
+
+//     shouldComponentUpdate(nextProps,nextState){
+//         if(equalObj(this.props,nextProps) && equalObj(this.state,nextState)){
+//             return false
+//         }
+//         return true
+//     }
+
+
+//     render() {
+//         console.log('Task render');
+//         return (
+//             <li className={this.props.isFinish ? 'finish':'default'}>
+//                 {this.props.name}                  
+//             </li>
+//         )
+//     }
+// }
