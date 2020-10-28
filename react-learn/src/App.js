74799fbddd43c38d90837cca188395ade93856ed
saybot <saybot@saybotdeMac-mini.local>
2020-10-28 18:12:29 +0800

    111

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,41 +1,11 @@
 import React, { Component } from 'react';
-import ErrorBound from './components/ErrorBound'
-
-function getData(){
-    return;
-}
-
-function A(){
-    return (
-        <>
-            <h2>我是A组件</h2>
-            <B/>
-        </>
-    )
-}
-
-class B extends Component{
-    render(){
-        return (
-            <>
-                <h2 onClick={()=>{
-                    throw new Error('asd')
-                }}>我是B组件</h2>
-                <h2>{getData().err}</h2>
-            </>
-        )
-    }
-}
-
 
 
 class App extends Component {
     render() {
         return (
             <div>
-                <ErrorBound>
-                    <A/>
-                </ErrorBound>
+                hello
             </div>
         );
     }
