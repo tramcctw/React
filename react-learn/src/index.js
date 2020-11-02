77dbc441ec02be3f00807afd1c95937912719c5b
saybot <saybot@saybotdeMac-mini.local>
2020-11-02 18:11:54 +0800

    1

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+// import App from './App'
 
-const app = <div>
-  <h1>
-    标题
-    {['abc',null,<p>段落</p> ]}
-  </h1>
-   <p>
-     {undefined}
-   </p>
-</div>
+// const app = <div>
+//   <h1>
+//     标题
+//     {['abc',null,<p>段落</p> ]}
+//   </h1>
+//    <p>
+//      {undefined}
+//    </p>
+// </div>
 
-console.log(app)
+// console.log(app)
 
-
-function  Comp1(props) {
-  return <h1>Comp1 {props.n}</h1>
+class App extends React.Component{
+  state = {
+    a:1
+  }
+  render(){
+    return <h2>{this.state.a}</h2>
+  }
 }
+
+console.log(new App())
+
+// function  Comp1(props) {
+//   return <h1>Comp1 {props.n}</h1>
+// }
 
 // function  App(props) {
 //   return (
