b4ff0257012014186e6d73830d67ae0914a840b5
tramcctw <tramcctw@foxmail.com>
2020-11-13 00:10:41 +0800

    1

--- react-learn/src/components/Jump.js
+++ react-learn/src/components/Jump.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { push } from 'connected-react-router'
+// push这里会返回一个action对象而不是调用push
+// 可以使用history.push跳转也可以
 
 function Jump({go,goA,goC}) {
     return (
