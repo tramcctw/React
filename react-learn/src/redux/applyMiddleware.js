29d81815b8699307a21161ca252dbc609379a6bc
tianwenchang <tianwenchang@huohua.cn>
2021-07-07 20:49:15 +0800

    手写

--- react-learn/src/redux/applyMiddleware.js
+++ react-learn/src/redux/applyMiddleware.js
@@ -13,6 +13,9 @@ export default function applyMiddleware(...args) {
         dispatch: store.dispatch,
       }
 
+      // 这里的simpleStore里面的dispatch要传入的是包装好的dispatch
+      // 但是不传simpleStore怎么得到包装好的dispatch呢
+
       const mids = args.map(mid => mid(simpleStore))
 
       const dispatch = compose(...mids)(store.dispatch)
